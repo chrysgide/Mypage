20e5cf30780b5037b206f6181e8d032b87871088
chrys gide <chrysgide26@gmail.com>
2018-04-23 09:29:50 +0200

    Page de login

---
+++ footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PERSONAL WEB PAGE</title>
+</head>
+<body style="margin:0">
+	<div style="margin: auto; width: 100%; height:20px; background-color: #a4faab; line-height:0px; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px; padding-bottom: 1vh; padding-top: 1vh; bottom:0; position: fixed; ">
+		<div><p style=" text-align:center; font-weight:none; color:black; line-height:0px; font-size:15px; margin-top: 2vh; margin-bottom: 2vh; font-size: 0.7em; font-weight: bold;"> @copy Academic year 2017/2018******* Chrys Gide TCHEUMADJEU*******MANIPAL UNIVERSITY/MIT/SOIS.</p></div>
+	</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PERSONAL WEB PAGE</title>
+	
+</head>
+<body>
+	<div style="margin-top:0 ; width: 100%; height: 75px; background-color: #a4faab; border-top-left-radius: 20px; border-top-right-radius: 20px; padding-top: 1vh; padding-bottom: 1vh;">
+		
+			
+		<p style="text-align: center; line-height:0px; font-size: 2.8em; font-family: Times New Roman;/*border:solid;">My page</p>
+		
+	</div>
+	
+
+</body>
+</html>
